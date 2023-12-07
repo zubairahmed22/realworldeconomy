dccd97f883c50917c8c5e003f75ecef1439069bc
unknown <zubairwastibaloch@gmail.com>
2023-12-07 12:50:22 +0500

    new chakra ui card added

--- src/Component/firstSection.js
+++ src/Component/firstSection.js
@@ -1,10 +1,11 @@
 import React,{useEffect} from 'react'
-import { Image, Box, Link,chakra,Flex} from '@chakra-ui/react'
+import { Image, Box, Link,chakra,Flex,SimpleGrid, WrapItem} from '@chakra-ui/react'
 
 import "jquery-ui-dist/jquery-ui"
 import $ from "jquery"
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import { wrap } from 'framer-motion';
 
 const FirstSection = () => {
 
@@ -17,8 +18,8 @@ const FirstSection = () => {
   return (
     <section className="services-section__block pt-90 pb-70 ptb-xs-60">
 		      <div className="container">
-			  <div className="row">
-						<div className="col-sm-4">
+			  
+						{/* <div className="col-sm-4">
 							<div className="about-block img-scale  mb-xs-40 clearfix">
 								<figure>
 									<a href="#"><img className="img-responsive" src="assets/images/services/value_proposition.png" alt="Photo"/></a>
@@ -98,10 +99,275 @@ Our goal is to facilitate investment in secure assets that might be otherwise in
 									</div>
 								</div>
 							</div>
-						</div>
+						</div> */}
+						<SimpleGrid columns={3}  py={6} ml={10}
+            minChildWidth="250px"
+            spacing={20}
+            flexWrap="wrap"
+            
+  
+>
+  <Box
+    mx="auto"
+	  flexBasis="33.333333%"
+    rounded="lg"
+    shadow="md"
+    bg="white"
+    _dark={{
+      bg: "gray.800",
+    }}
+    maxW="3xl"
+  >
+    
+    <Image
+      roundedTop="lg"
+      w="full"
+      h={64}
+      fit="cover"
+      src="assets/images/services/value_proposition.png" 
+      alt="Article"
+    />
+    <Box p={6}>
+      <Box>
+        
+        <Link
+          display="block"
+          color="gray.800"
+          _dark={{
+            color: "white",
+          }}
+          fontWeight="bold"
+          fontSize="2xl"
+          mt={2}
+          _hover={{
+            color: "gray.600",
+            textDecor: "underline",
+          }}
+        >
+         Unique Value Proposition
+        </Link>
+        <chakra.p
+          mt={2}
+          fontSize="16"
+          color="gray.600"
+          _dark={{
+            color: "gray.400",
+          }}
+        >
+       Our investments are based solely on real assets such as gold, silver, real estate, and thriving businesses.
+        By utilizing cryptocurrency and blockchain technology, we enable anyone, even those with modest means,
+			 to participate in investments, contributing to greater democratization of the investment world.
+        </chakra.p>
+      </Box>
+
+      <Box mt={4}>
+        <Flex alignItems="center">
+          <Flex alignItems="center">
+           
+            
+          </Flex>
+          
+        </Flex>
+      </Box>
+    </Box>
+  </Box>
 
 
-					</div>
+  <Box
+    mx="auto"
+	  flexBasis="33.333333%"
+    rounded="lg"
+    shadow="md"
+    bg="white"
+    _dark={{
+      bg: "gray.800",
+    }}
+    maxW="2xl"
+  >
+    <Image
+      roundedTop="lg"
+      w="full"
+      h={64}
+      fit="cover"
+      src="assets/images/services/goldpng.png"
+    />
+
+    <Box p={6}>
+      <Box>
+        
+        <Link
+          display="block"
+          color="gray.800"
+          _dark={{
+            color: "white",
+          }}
+          fontWeight="bold"
+          fontSize="2xl"
+          mt={2}
+          _hover={{
+            color: "gray.600",
+            textDecor: "underline",
+          }}
+        >
+       Concept of Gold-Backed Cryptocurrency
+        </Link>
+        <chakra.p
+          mt={2}
+          fontSize="medium"
+          color="gray.600"
+          _dark={{
+            color: "gray.400",
+          }}
+        >
+          One of our long-term goals is to create a gold-backed currency, providing a stable solution for countries facing inflationary issues.
+           We aim to base the currency's value on real assets,
+					eliminating the need to rely on the arbitrary value of paper money.
+        </chakra.p>
+      </Box>
+
+      <Box mt={4}>
+        <Flex alignItems="center">
+          
+          
+        </Flex>
+      </Box>
+    </Box>
+  </Box>
+  <Box
+    mx="auto"
+	  flexBasis="33.333333%"
+    rounded="lg"
+    shadow="md"
+    bg="white"
+    _dark={{
+      bg: "gray.800",
+    }}
+    maxW="2xl"
+  >
+    <Image
+      roundedTop="lg"
+      w="full"
+      h={64}
+      fit="cover"
+      src="assets/images/services/new_goal_pic.png"
+      alt="Article"
+    />
+
+    <Box p={6}>
+      <Box>
+        
+        <Link
+          display="block"
+          color="gray.800"
+          _dark={{
+            color: "white",
+          }}
+          fontWeight="bold"
+          fontSize="2xl"
+          mt={2}
+          _hover={{
+            color: "gray.600",
+            textDecor: "underline",
+          }}
+        >
+         Company Goal
+        </Link>
+        <chakra.p
+          mt={2}
+          fontSize="medium"
+          color="gray.600"
+          _dark={{
+            color: "gray.400",
+          }}
+        >
+       Our goal is to facilitate investment in secure assets that might be otherwise inaccessible to many.
+ We want everyone, regardless of location or the amount of available funds, to participate in investments that generate stable profits.
+
+        </chakra.p>
+      </Box>
+
+      <Box mt={4}>
+        <Flex alignItems="center">
+          <Flex alignItems="center">
+           
+          </Flex>
+         
+        </Flex>
+      </Box>
+    </Box>
+  </Box>
+  <Box
+    mx="auto"
+    flexBasis="33.333333%"
+	
+    rounded="lg"
+    shadow="md"
+    bg="white"
+    _dark={{
+      bg: "gray.800",
+    }}
+    maxW="2xl"
+  >
+    
+    <Image
+      roundedTop="lg"
+      w="full"
+      h={64}
+      fit="cover"
+      src="assets/images/services/newapproch.jpg"
+      alt="Article"
+    />
+    <Box p={6}>
+      <Box>
+        
+        <Link
+          display="block"
+          color="gray.800"
+          _dark={{
+            color: "white",
+          }}
+          fontWeight="bold"
+          fontSize="2xl"
+          mt={2}
+          _hover={{
+            color: "gray.600",
+            textDecor: "underline",
+          }}
+        >
+         New Approach to Investments
+        </Link>
+        <chakra.p
+          mt={2}
+          fontSize="medium"
+          color="gray.600"
+          _dark={{
+            color: "gray.400",
+          }}
+        >
+         Real World Invest Company seeks to transform how people invest by eliminating speculation in favor of secured,
+										 real assets. We reject investments in cryptocurrencies based solely on code, 
+										focusing instead on investments with real value and the potential for profits.
+        </chakra.p>
+      </Box>
+
+      <Box mt={4}>
+        <Flex alignItems="center">
+          <Flex alignItems="center">
+           
+            
+          </Flex>
+          
+        </Flex>
+      </Box>
+    </Box>
+  </Box>
+
+  
+
+
+</SimpleGrid>;
+
+					
 		</div>
 		</section>
   )
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ChakraProvider } from "@chakra-ui/react";
 
 import { BrowserRouter } from 'react-router-dom';
 
@@ -12,7 +13,9 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
 
+<ChakraProvider>
     <App />
+   </ChakraProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
